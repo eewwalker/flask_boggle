b00c66f3555d6e126bf431f12dbd5824c87e8552
Emily Walker <ewalker415@gmail.com>
2024-02-23 16:59:27 -0800

    score word

--- app.py
+++ app.py
@@ -35,3 +35,21 @@ def new_game():
     new_game_info = {'gameId': game_id, 'board': game.board}
 
     return jsonify(new_game_info)
+
+
+@app.post('/api/score-word')
+def score_word():
+
+    game_id = request.json['gameId']
+    curr_word = request.json['wordInput']
+    game = games[game_id]
+    word_list = game.word_list
+    result = {'result': "not-word"}
+    breakpoint()
+    if game.check_word_on_board(curr_word):
+        if word_list.check_word(curr_word):
+            result['result'] = 'ok'
+    else:
+        result['result'] = 'not-on-board'
+
+    return jsonify(result)
